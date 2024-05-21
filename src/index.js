e1d306301cffec1820f7decc3f5c258ae9eb8db8
gothwavy <ryanalmeidadev@gmail.com>
2024-05-21 00:09:03 -0300

    v1.0✔

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Contador from './Contador';
+import ListaTarefas from './ListaTarefas';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Contador />
+    <ListaTarefas />
   </React.StrictMode>
 );
